d7b34ddde46a28af7e5aeaa00dc2eb55088bb9ca
Ali Haider <alihaider@Alis-MacBook-Pro.local>
2020-05-09 14:34:16 +0500

    week 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div
+          style={{ width: "100%", height: 50, backgroundColor: "green" }}
+        ></div>
+        <div
+          style={{
+            width: "100%",
+            height: 300,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-start",
+            backgroundColor: "red",
+            flexDirection: "row",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <h1>I am ALI</h1>
+          <h1>I am ALI</h1>
+        </div>
 
-export default App;
+        <div className="alert alert-success" role="alert">
+          <h4 className="alert-heading">Well done!</h4>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
